03b3be34f59f790e6362c0adc4aa62cc3e2b69cc
Deniz <denizguzel.iu@gmail.com>
2016-11-23 00:54:27 +0300

    Gereksiz dosyaları sildim.

--- index.php
+++ index.php
@@ -10,29 +10,34 @@
   <link rel="stylesheet" type="text/css" href="css/materialdesignicons.min.css">
   <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
-<body>
-  <div class="container">
-    <nav class="navbar navbar-light bg-faded">
-      <ul class="nav navbar-nav">
-        <li class="nav-item"><a class="nav-link" href="anasayfa.php">Giriş</a></li>
-        <li class="nav-item"><a class="nav-link" href="pass-recovery.php">Şifremi Unuttum</a></li>
-        <li class="nav-item"><a class="nav-link" href="profile.php">Profil</a></li>
-        <li class="nav-item"><a class="nav-link" href="dashboard.php">Dashboard</a></li>
-        <li class="nav-item"><a class="nav-link" href="user.php">Kullanıcılar</a></li>
-        <li class="nav-item"><a class="nav-link" href="user-create.php">Kullanıcı Oluştur</a></li>
-        <li class="nav-item"><a class="nav-link" href="user-detail.php">Kullanıcı Detay</a></li>
-        <li class="nav-item"><a class="nav-link" href="department-create.php">Departmanlar</a></li>
-        <li class="nav-item"><a class="nav-link" href="unit-create.php">Birimler</a></li>
-        <li class="nav-item"><a class="nav-link" href="role-create.php">Roller</a></li>
-        <li class="nav-item"><a class="nav-link" href="ability-create.php">Yetenekler</a></li>
-        <li class="nav-item"><a class="nav-link" href="course.php">Eğitimler</a></li>
-        <li class="nav-item"><a class="nav-link" href="course-create.php">Eğitim Oluştur</a></li>
-        <li class="nav-item"><a class="nav-link" href="course-detail.php">Eğitim Detay</a></li>
-        <li class="nav-item"><a class="nav-link" href="course-instructor.php">Eğitmenler</a></li>
-        <li class="nav-item"><a class="nav-link" href="course-users.php">Eğitimdeki Kullanıcılar</a></li>
-        <li class="nav-item"><a class="nav-link" href="statistics.php">İstatistikler</a></li>
-        <li class="nav-item"><a class="nav-link" href="ability-change.php">Yetenek Seviyeleri</a></li>
-      </ul>
-    </nav>
+<body class="login">
+<div class="container">
+  <div class="row">
+    <div class="col-xs-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-xl-4 offset-xl-4">
+      <form action="">
+        <div class="card card-login">
+          <div class="card-header"><span class="highlight">Roleaby</span> Admin</div>
+          <div class="card-block">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Kullanıcı Adı">
+              <i class="mdi mdi-account"></i>
+            </div>
+            <div class="form-group">
+              <input type="password" class="form-control" placeholder="********">
+              <i class="mdi mdi-key"></i>
+            </div>
+            <div class="form-group">
+              <button type="submit" class="btn btn-success">Giriş Yap</button>
+            </div>
+            <div class="form-group">
+              <input id="remember" type="checkbox" class="form-control">
+              <label for="remember">Beni Hatırla</label>
+              <a href="#" class="forgot">Şifremi Unuttum</a>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
   </div>
+</div>
 <?php include 'footer.php' ?>
