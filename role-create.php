d9fb8a2b73a44aaafc5c38b8a9d9723e6391925a
Okan <okan.uzun@outlook.com>
2016-11-24 22:22:08 +0300

    updating role added

--- role-create.php
+++ role-create.php
@@ -12,16 +12,23 @@
     oci_bind_by_name($stmt,':dep_id',$dep_id);
     oci_bind_by_name($stmt,':is_valid',$message);
 
+
+    if (isset($_POST["unit_id"])){
+      $unit_id = $_POST["unit_id"];  
+    }
+
+    if (isset($_POST["dep_id"])){
+      $dep_id = $_POST["dep_id"];  
+    }
+
     $role_name = $_POST["role_name"];
-    $unit_id = $_POST["unit_id"];
-    $dep_id = $_POST["dep_id"];
 
     oci_execute($stmt);
     //echo "$message\n";
   }
 
   else if (isset($_POST["update-role"])){
-    $sql = 'BEGIN SP_UPDATE_ROLE(:rle_name,:unt_id,:dep_id,:is_valid); END;';
+    $sql = 'BEGIN SP_UPDATE_ROLE(:rle_id,:rle_name,:unt_id,:dep_id,:is_valid); END;';
     $stmt = oci_parse($conn,$sql);
 
     oci_bind_by_name($stmt,':rle_id',$role_id);
@@ -30,7 +37,7 @@
     oci_bind_by_name($stmt,':dep_id',$dep_id);
     oci_bind_by_name($stmt,':is_valid',$message);
 
-    $role_name = $_POST["rle_id"];
+    $role_id = $_POST["role_id"];
     $role_name = $_POST["role_name"];
     $unit_id = $_POST["unit_id"];
     $dep_id = $_POST["dep_id"];
@@ -72,7 +79,7 @@
                   $stmt = oci_parse($conn,$sql);
                   $r = oci_execute($stmt);
                   echo '<select id="roleDepartment" name="dep_id" class="form-control selectpicker" data-live-search="true" data-size="5" data-width="auto" title="Bağlı Olduğu Departmanı Seçiniz">';
-                  echo '<option value ="Seçiniz">Seçiniz</option>';
+                  echo '<option>Yok</option>';
                   while ($row = oci_fetch_array($stmt, OCI_RETURN_NULLS+OCI_ASSOC))
                     echo '<option value ="'.$row["PK"].'">'.$row["DEPARTMENT_NAME"].'</option>';
                   echo '</select>';
@@ -85,6 +92,7 @@
                   $stmt = oci_parse($conn,$sql);
                   $r = oci_execute($stmt);
                   echo '<select id="roleUnit" name="unit_id" class="form-control selectpicker" data-live-search="true" data-size="5" data-width="auto" title="Bağlı Olduğu Birimi Seçiniz">';
+                  echo '<option>Yok</option>';
                   while ($row = oci_fetch_array($stmt, OCI_RETURN_NULLS+OCI_ASSOC))
                     echo '<option value ="'.$row["PK"].'">'.$row["UNIT_NAME"].'</option>';
                   echo '</select>';
@@ -106,7 +114,8 @@
               <tbody>
               <?php
               include "dbsettings.php";
-              $sql = 'SELECT T_ROLE.PK,T_ROLE.ROLE_NAME,(SELECT COUNT(T_USER.PK) FROM T_USER
+              $sql = 'SELECT T_ROLE.PK,T_ROLE.ROLE_NAME,T_DEPARTMENT.DEPARTMENT_NAME AS DEP_NAME,T_UNIT.UNIT_NAME AS U_NAME,
+              (SELECT COUNT(T_USER.PK) FROM T_USER
               WHERE T_USER.ROLE_FK = T_ROLE.PK) AS X,T_UNIT.PK AS U_PK,T_DEPARTMENT.PK AS DEP_PK FROM T_ROLE
               LEFT JOIN T_UNIT ON T_ROLE.UNIT_FK = T_UNIT.PK
               LEFT JOIN T_DEPARTMENT ON T_ROLE.DEPARTMENT_FK = T_DEPARTMENT.PK';
@@ -118,7 +127,7 @@
                      echo '<td>'.$row['X'].'</td>';
                      echo '
                      <td class="text-xs-center">
-                      <a href="#updateModal" class="table-icon" rel="tooltip" title="Güncelle" data-toggle="modal" data-id="'.$row['PK'].'" data-name="'.$row['ROLE_NAME'].'" data-unit="Yazılım Birimi" data-department="Bilişim Departmanı"><i class="mdi mdi-autorenew"></i></a>
+                      <a href="#updateModal" class="table-icon" rel="tooltip" title="Güncelle" data-toggle="modal" data-id="'.$row['PK'].'" data-name="'.$row['ROLE_NAME'].'" data-unit="'.$row['U_NAME'].'" data-department="'.$row['DEP_NAME'].'"><i class="mdi mdi-autorenew"></i></a>
                       <a href="#deleteModal" class="table-icon" rel="tooltip" title="Sil" data-toggle="modal" data-id="'.$row['PK'].'"><i class="mdi mdi-delete"></i></a>
                      </td>';
                  echo '<tr>';
@@ -145,22 +154,34 @@
           <div class="modal-body">
             <div class="form-group">
               <label for="updateName" class="form-control-label">Rol Adı:</label>
-              <input type="text" class="form-control" id="updateName">
+              <input type="text" class="form-control" id="updateName" name="role_name">
               <input type="hidden" name="role_id" id="role_id">
             </div>
             <div class="form-group">
               <label for="updateDepartmentSelect" class="form-control-label">Departman:</label>
-              <select id="updateDepartmentSelect" class="form-control selectpicker" data-live-search="true" data-size="5" title="Bağlı Olduğu Departmanı Seçiniz">
-                <option value="Bilişim Departmanı">Bilişim Departmanı</option>
-                <option value="Halkla İlişkiler Departmanı">Halkla İlişkiler Departmanı</option>
-              </select>
+                  <?php
+                    include "dbsettings.php";
+                    $sql = 'SELECT PK,DEPARTMENT_NAME FROM T_DEPARTMENT';
+                    $stmt = oci_parse($conn,$sql);
+                    $r = oci_execute($stmt);
+                    echo '<select id="updateDepartmentSelect" name="dep_id" class="form-control selectpicker" data-live-search="true" data-size="5">';
+                    while ($row = oci_fetch_array($stmt, OCI_RETURN_NULLS+OCI_ASSOC))
+                      echo '<option value ="'.$row["PK"].'">'.$row["DEPARTMENT_NAME"].'</option>';
+                    echo '</select>';
+                  ?>
             </div>
             <div class="form-group">
               <label for="updateUnitSelect" class="form-control-label">Birim:</label>
-              <select id="updateUnitSelect" class="form-control selectpicker" data-live-search="true" data-size="5" title="Bağlı Olduğu Birimi Seçiniz">
-                <option value="Ağ Birimi">Ağ Birimi</option>
-                <option value="Yazılım Birimi">Yazılım Birimi</option>
-              </select>
+                  <?php
+                    include "dbsettings.php";
+                    $sql = 'SELECT PK,UNIT_NAME FROM T_UNIT';
+                    $stmt = oci_parse($conn,$sql);
+                    $r = oci_execute($stmt);
+                    echo '<select id="updateUnitSelect" name="unit_id" class="form-control selectpicker" data-live-search="true" data-size="5">';
+                    while ($row = oci_fetch_array($stmt, OCI_RETURN_NULLS+OCI_ASSOC))
+                      echo '<option value ="'.$row["PK"].'">'.$row["UNIT_NAME"].'</option>';
+                    echo '</select>';
+                  ?>
             </div>
           </div>
           <div class="modal-footer">
