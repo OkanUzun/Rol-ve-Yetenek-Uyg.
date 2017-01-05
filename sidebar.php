323f18bd491dbd2c2f33e64a09b81f405315da49
Okan <okan.uzun@outlook.com>
2017-01-06 02:57:12 +0300

    dynamic active class for current page

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,12 @@
+<?php  
+  function contain_page($page_link){
+    if ((strpos($_SERVER['REQUEST_URI'],$page_link)) !== false) {
+      return "active";
+    }
+    else 
+      return "";
+  }
+?>
 <div class="side-navigation mCustomScrollbar">
   <div class="sidebar-header">
     <a class="sidebar-brand" href="dashboard.php" title="Ana Sayfa"><span class="highlight">Roleaby v1</span></a>
@@ -11,61 +20,61 @@
           <button type="submit"><i class="mdi mdi-magnify"></i></button>
         </form>
       </li>
-      <li class="active">
+      <li class="<?php print(contain_page('dashboard.php'))?>">
         <a href="dashboard.php">
           <div class="icon"><i class="mdi mdi-view-dashboard"></i></div>
           <div class="title">Ana Sayfa</div>
         </a>
       </li>
-      <li>
+      <li class="<?php print(contain_page('user.php'))?>">
         <a href="user.php">
           <div class="icon"><i class="mdi mdi-account-multiple"></i></div>
           <div class="title">Kullanıcılar</div>
         </a>
       </li>
-      <li>
+      <li class="<?php print(contain_page('department-create.php'))?>">
         <a href="department-create.php">
           <div class="icon"><i class="mdi mdi-library"></i></div>
           <div class="title">Departmanlar</div>
         </a>
       </li>
-      <li>
+      <li class="<?php print(contain_page('unit-create.php'))?>">
         <a href="unit-create.php">
           <div class="icon"><i class="mdi mdi-library-books"></i></div>
           <div class="title">Birimler</div>
         </a>
       </li>
-      <li>
+      <li class="<?php print(contain_page('role-create'))?>">
         <a href="role-create.php">
           <div class="icon"><i class="mdi mdi-account-check"></i></div>
           <div class="title">Roller</div>
         </a>
       </li>
-      <li>
+      <li class="<?php print(contain_page('ability-create'))?>">
         <a href="ability-create.php">
           <div class="icon"><i class="mdi mdi-account-star-variant"></i></div>
           <div class="title">Yetenekler</div>
         </a>
       </li>
-      <li>
+      <li class="<?php print(contain_page('course.php'))?>">
         <a href="course.php">
           <div class="icon"><i class="mdi mdi-book-open-page-variant"></i></div>
           <div class="title">Eğitimler</div>
         </a>
       </li>
-      <li>
+      <li class="<?php print(contain_page('course-instructor.php'))?>">
         <a href="course-instructor.php">
           <div class="icon"><i class="mdi mdi-account-switch"></i></div>
           <div class="title">Eğitmenler</div>
         </a>
       </li>
-      <li>
+      <li class="<?php print(contain_page('search.php'))?>">
         <a href="search.php">
           <div class="icon"><i class="mdi mdi-account-search"></i></div>
           <div class="title">Arama</div>
         </a>
       </li>
-      <li>
+      <li class="<?php print(contain_page('statistics.php'))?>">
         <a href="statistics.php">
           <div class="icon"><i class="mdi mdi-chart-bar"></i></div>
           <div class="title">İstatistikler</div>
